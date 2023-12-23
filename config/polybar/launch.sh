a1c57c7d902e97e9e36f3563636f4ae422599f51
Favianl <leofavianf@gmail.com>
2023-12-23 15:53:11 -0500

    config. polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+
+# killall -q polybar
+
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar myBar 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar launched..."
+# polybar myBar 2>&1 | tee -a /tmp/polybar.log & disown
+
+# echo "Polybar launched..."
+
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		(MONITOR=$mon polybar -q pam1 -c config.ini)&
+		(MONITOR=$mon polybar -q pam2 -c config.ini)&
+		(MONITOR=$mon polybar -q pam3 -c config.ini)&
+		(MONITOR=$mon polybar -q pam4 -c config.ini)&
+		(MONITOR=$mon polybar -q pam5 -c config.ini)&
+		(MONITOR=$mon polybar -q pam6 -c config.ini)&
+	done
