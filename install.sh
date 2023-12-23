12cbf455542024de24081f5030c5e86959a11310
Favianl <leofavianf@gmail.com>
2023-12-23 14:47:24 -0500

    install changed

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 
 #!/bin/bash
 #
-sudo pacman -S bspwm polybar kitty rofi sxhkd nemo neofetch htop xf86-video-vesa xf86-video-ati xf86-video-amdgpu vulkan-radeon vulkan-icd-loader xorg-server xorg-xinit xorg-xev xorg mesa mesa-demos gvfs leafpad picom feh lxappearance lxsession arandr neovim lsd bat vlc gpicview evince zip unzip unrar firefox base-devel yad xdotool imagemagick drawing inkscape redshift tree xclip ttf-jetbrains-mono-nerd python3 python-pip locate stylua --noconfirm
+#sudo pacman -S bspwm polybar kitty rofi sxhkd nemo nemo-fileroller neofetch htop fzf xf86-video-vesa xf86-video-ati xf86-video-amdgpu vulkan-radeon vulkan-icd-loader xorg-server xorg-xinit xorg-xev xorg mesa mesa-demos gvfs ntfs-3g leafpad picom feh lxappearance arandr neovim lsd bat vlc gpicview evince zip unzip unrar firefox base-devel yad xdotool imagemagick drawing inkscape redshift tree xclip ttf-jetbrains-mono-nerd python3 python-pip locate stylua discord telegram-desktop veracrypt scrcpy obs-studio noise-suppression-for-voice mariadb dbeaver mpv --noconfirm
+
+sudo pacman -S bspwm polybar kitty rofi sxhkd nemo nemo-fileroller neofetch lightdm lightdm-gtk-greeter htop fzf xf86-video-vesa xf86-video-ati xf86-video-amdgpu vulkan-radeon vulkan-icd-loader xorg-server xorg-xinit xorg-xev xorg mesa mesa-demos gvfs ntfs-3g leafpad picom feh lxappearance arandr neovim lsd bat firefox base-devel redshift xclip ttf-jetbrains-mono-nerd python3 python-pip locate stylua --noconfirm
 
 cp -r config/* ~/.config/
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" &
+wait
+echo "Oh My Bash installed..."
 
+echo "Copying home files..."
+sleep 3
 rm ~/.bashrc
 cp -r home/. ~/
 
@@ -14,9 +20,5 @@ sudo systemctl enable lightdm
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-
-
-
-
-echo "============FIN==============="
+echo "============ End ==============="
 
