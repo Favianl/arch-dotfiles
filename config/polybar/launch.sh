bf0b9d4adb210c06ab0e6f1451b30a3175f3dd30
Favianl <leofavianf@gmail.com>
2023-12-30 14:05:58 -0500

    changes polybar and more

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,22 +3,7 @@
 # Terminar cualquier instancia en ejecución de Polybar
 killall -q polybar
 
-# Establecer la ruta del archivo config.ini
-config_file="$HOME/.config/polybar/config.ini"
-
-# Lanzar Polybar con la configuración de tu amigo
-launch_bars() {
-    for mon in $(polybar --list-monitors | cut -d":" -f1); do
-        (MONITOR=$mon polybar -q pam1 -c "$config_file")&
-        (MONITOR=$mon polybar -q pam2 -c "$config_file")&
-        (MONITOR=$mon polybar -q pam3 -c "$config_file")&
-        (MONITOR=$mon polybar -q pam4 -c "$config_file")&
-        (MONITOR=$mon polybar -q pam5 -c "$config_file")&
-        (MONITOR=$mon polybar -q pam6 -c "$config_file")&
-    done
-}
-
 # Lanzar Polybar utilizando la configuración de tu amigo
-launch_bars 2>&1 | tee -a /tmp/polybar.log & disown
+polybar myBar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Launch Polybar..."
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #
 #sudo pacman -S bspwm polybar kitty rofi sxhkd nemo nemo-fileroller neofetch htop fzf xf86-video-vesa xf86-video-ati xf86-video-amdgpu vulkan-radeon vulkan-icd-loader xorg-server xorg-xinit xorg-xev xorg mesa mesa-demos gvfs ntfs-3g leafpad picom feh lxappearance arandr neovim lsd bat vlc gpicview evince zip unzip unrar firefox base-devel yad xdotool imagemagick drawing inkscape redshift tree xclip ttf-jetbrains-mono-nerd python3 python-pip locate stylua discord telegram-desktop veracrypt scrcpy obs-studio noise-suppression-for-voice mariadb dbeaver mpv --noconfirm
 
-sudo pacman -S bspwm polybar kitty rofi sxhkd nemo nemo-fileroller neofetch lightdm lightdm-gtk-greeter htop fzf xf86-video-vesa xf86-video-ati xf86-video-amdgpu vulkan-radeon vulkan-icd-loader xorg-server xorg-xinit xorg-xev xorg mesa mesa-demos gvfs ntfs-3g leafpad picom feh lxappearance arandr neovim lsd bat firefox base-devel redshift xclip ttf-jetbrains-mono-nerd python3 python-pip locate stylua --noconfirm
+sudo pacman -S bspwm polybar kitty rofi sxhkd nemo nemo-fileroller neofetch lightdm lightdm-gtk-greeter htop fzf xf86-video-vesa xf86-video-ati xf86-video-amdgpu vulkan-radeon vulkan-icd-loader xorg-server xorg-xinit xorg-xev xorg mesa mesa-demos gvfs ntfs-3g leafpad picom feh lxappearance arandr neovim lsd bat firefox base-devel redshift xclip ttf-jetbrains-mono-nerd python3 python-pip locate stylua calcurse pavucontrol pipewire-pulse papirus-icon-theme gtk3 gtk4 --noconfirm
 
 cp -r config/* ~/.config/
 
